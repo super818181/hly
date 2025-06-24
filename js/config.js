88e6332c7cf1eca8ab21ddd057c540cd4759ab2a
super818181 <super818181@outlook.com>
2025-06-24 11:22:29 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,16 @@
+/*-------config start--------*/
+var _wip = 'wlgf.panguh5.com';  //t1
+var _port = '1714';
+/*--------------config end-------------*/
+
+var BaseUrl = 'http://' + _wip + ':' + _port;
+var ResourceUrl = '/';
+var Suffix = '.html';
+var _isTx = 0;
+var _wport = '10001';
+
+BaseUrl = 'http://wlgf.panguh5.com:2998/' + _port;
+_wport = '10001';
+
+var _config = BaseUrl + '/wx/oss?p='+_port;
+var _OssUrl = '';
